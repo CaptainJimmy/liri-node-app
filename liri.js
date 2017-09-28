56a0c7b3a386cb535541a84adc0d137f012fe3c3
Jim Reinknecht <captainjimscharters@gmail.com>
2017-09-28 16:36:47 -0400

    ran through lint

--- liri.js
+++ liri.js
@@ -1,43 +1,47 @@
+var fs = require("fs");
+var moment = require("moment");
+var params;
+var search1;
+var search2;
+var movieName;
+var i;
+var timestamp = moment().format();
 //prep the incoming arguments
 
 if (process.argv[2]) {
     var arg2 = process.argv[2].toLowerCase();
-};
+}
 if (process.argv[3]) {
     var arg3 = process.argv[3].toLowerCase();
-};
+}
 if (process.argv[4]) {
     var arg4 = process.argv[4].toLowerCase();
-};
+}
 
 
 //check to make sure there are arguments. if not, return syntax.
 if (!arg2) {
     console.log("No arguments detected.  Acceptable syntax is: [spotify artist|track 'search criteria'], [twitter screen_name], [movieinfo 'moviename'], [random]");
-} 
-else {
-main(arg2,arg3,arg4);
+} else {
+    main(arg2, arg3, arg4);
 
 }
 
 
-function main(arg2,arg3,arg4){
-		//log this
+function main(arg2, arg3, arg4) {
+    //log this
 
-	    var fs = require("fs");
-	    var moment=require("moment");
-	    var timestamp=moment().format()
+    "use strict";
+    fs.appendFile("output.txt", timestamp + "," + arg2 + "," + arg3 + "," + arg4 + '\n', function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
 
-	    	    fs.appendFile("output.txt",timestamp+","+arg2+","+arg3+","+arg4+'\n', function(err) {
-    		  if (err) {
-    				return console.log(err);
-  						};
-  					});
     switch (arg2) {
 
         case "twitter":
             //if no screen name exists, set a default'
-            var params;
             if (!arg3) {
                 params = {
                     screen_name: 'engadget',
@@ -50,7 +54,7 @@ function main(arg2,arg3,arg4){
                     count: 5,
                     include_rts: 0
 
-                }
+                };
             }
 
             getTweets(params);
@@ -59,10 +63,10 @@ function main(arg2,arg3,arg4){
 
 
         case "spotify":
-            var search1;
-            var search2;
+
             if (!arg3) {
-                "Invalid Syntax. Acceptable syntax is [spotify artist|track 'artist or track'], [twitter screen_name], [movieinfo 'moviename'], [random]"
+                console.log(
+                    "Invalid Syntax. Acceptable syntax is [spotify artist|track 'artist or track'], [twitter screen_name], [movieinfo 'moviename'], [random]");
             }
             // If No Search Term is given, one will be provided for you"
             else if (!arg4) {
@@ -73,15 +77,14 @@ function main(arg2,arg3,arg4){
                 search1 = arg3;
                 search2 = arg4;
             }
-            getSpotify(search1, search2)
+            getSpotify(search1, search2);
             break;
 
         case "movieinfo":
-            var movieName;
             //making sure arguement 3 exists. if it dosnt, it will default the movie PCU
             if (!arg3) {
                 console.log("No movie selected.  You are getting the information for the best movie of all time.");
-                movieName = "PCU"
+                movieName = "PCU";
             } else {
                 movieName = arg3;
             }
@@ -103,6 +106,7 @@ function main(arg2,arg3,arg4){
 
 function getMovieInfo(movieName) {
 
+    "use strict";
     var request = require('request');
     request("http://www.omdbapi.com/?t=" + arg3 + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
         if (!error && response.statusCode === 200) {
@@ -131,6 +135,7 @@ function getMovieInfo(movieName) {
 
 function getTweets(params) {
     //Keys and Info
+    "use strict";
     var twitterkey = require("./keys.js");
     var Twitter = require('twitter');
     var client = new Twitter(twitterkey);
@@ -138,11 +143,12 @@ function getTweets(params) {
     //poll from twitter
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-            for (var i = 0; i < tweets.length; i++) {
+            for (i = 0; i < tweets.length; i += 1) {
                 console.log(
                     "Created at: " + tweets[i].created_at + '\n',
                     "Created by: " + tweets[i].user.screen_name + '\n',
-                    "Tweet: " + tweets[i].text);
+                    "Tweet: " + tweets[i].text
+                );
             }
         }
 
@@ -152,6 +158,7 @@ function getTweets(params) {
 
 function getSpotify(search1, search2) {
 
+    "use strict";
     var Spotify = require("node-spotify-api");
     var spotify = new Spotify({
         id: "544396b737244f97971b4ce32dc2c0ef",
@@ -172,7 +179,7 @@ function getSpotify(search1, search2) {
 
         if (search1 === "track") {
             var dataReturn = data.tracks;
-            for (var i = 0; i < dataReturn.items.length; i++) {
+            for (i = 0; i < dataReturn.items.length; i += 1) {
 
                 console.log(
                     "Item #" + i + '\n',
@@ -180,14 +187,12 @@ function getSpotify(search1, search2) {
                     "Album Name: " + dataReturn.items[i].album.name + '\n',
                     "Track Name: " + dataReturn.items[i].name + '\n'
                 );
-                //console.log(dataReturn.items[i]);
 
             }
         } else {
             dataReturn = data.artists;
-            // console.log(dataReturn.length);
-            //      	console.log(dataReturn.items.length);
-            for (var i = 0; i < dataReturn.items.length; i++) {
+
+            for (i = 0; i < dataReturn.items.length; i += 1) {
                 console.log(
                     "Item #" + i + '\n',
                     "Artist: " + dataReturn.items[i].name + '\n',
@@ -199,20 +204,20 @@ function getSpotify(search1, search2) {
         }
 
     });
-};
+}
 
-function doRandomStuff(){
-    var fs = require("fs");
-    fs.readFile("random.txt","utf8", function(err,data) {
-    		  if (err) {
-    				return console.log(err);
-  						};
-  		 var randomStuff = data.split("\n");
-  		 var randomIndex = Math.floor(Math.random()*randomStuff.length);
-  		 var randomTask = randomStuff[randomIndex].trim();
- 		var newArgs=randomTask.split(",");
- 		console.log(newArgs);
- 		main(newArgs[0],newArgs[1],newArgs[2]);
-			});
+function doRandomStuff() {
+    "use strict";
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var randomStuff = data.split("\n");
+        var randomIndex = Math.floor(Math.random() * randomStuff.length);
+        var randomTask = randomStuff[randomIndex].trim();
+        var newArgs = randomTask.split(",");
+        console.log(newArgs);
+        main(newArgs[0], newArgs[1], newArgs[2]);
+    });
 
 }
